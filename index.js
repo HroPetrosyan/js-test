4209288e4b5d2d754c34ebc8351932bd6c46f004
HroPetrosyan <hrahat2000@gmail.com>
2022-03-24 00:43:43 +0400

    feat(winner): doesCartWinPrize

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const WINNING_SUM = 123.40;
 @param {Array} cartItemsPrices
 */
 function doesCartWinPrize(cartItemsPrices) {
-  
+    let sum = cartItemsPrices.reduce((aggr,value)=>{
+        return +aggr + +value
+    })
+    if(sum*10 === WINNING_SUM*10) return true 
+    return false
 }
 
 module.exports = doesCartWinPrize;
